5de3af607442dcb1106db2beec4196121b3a78f7
Sergio <diaz.sergio605@gmail.com>
2014-11-04 14:09:32 -0800

    Added posting.

--- app/custom.php
+++ app/custom.php
@@ -13,19 +13,88 @@ class Notes {
 
 	public function get_notes($uid = null) {
 
+		include("extras/design.php");
+
+		$Design = New Design;
+
 		if ($uid == null) {
 
-    		$query = $this->instance->query("SELECT * FROM notes");
+    		$query = $this->instance->query("SELECT * FROM notes ORDER BY time DESC");
 
     	    while ($note_arr = $query->fetch_array()) {
 
-				echo $note_arr['content'];
-				echo "<br>";
+				$paneldata = array(
+					'body' => htmlentities($note_arr['content']),
+					'title' => htmlentities($note_arr['username']),
+					'footer' => $this->timeago($note_arr['time'])
+					);
+				echo '<div class="note">';
+				echo $Design::panel($paneldata, true);
+				echo '</div>';
 
 			}
 
 		}
 
+	}
+
+	public function get_username($uid) {
+
+		$query = $this->instance->prepare("SELECT username FROM users WHERE userid=? LIMIT 1");
+		$query->bind_param("s", $uid);
+		$query->execute();
+    	$result = $query->get_result();
+    	$row = $result->fetch_array(MYSQLI_ASSOC);
+
+    	return htmlentities($row['username']);
+
+	}
+
+	public function create($array = array()) {
+
+		$query = $this->instance->prepare("INSERT into notes (content, username, time) VALUES (?, ?, ?)");
+		$query->bind_param("ssi", $array['content'], $array['username'], $array['time']);
+		$query->execute();
+    	$result = $query->get_result();
+    	$row = $result->fetch_array(MYSQLI_ASSOC);			
+
+	}
+
+	// function from http://stackoverflow.com/questions/1416697/converting-timestamp-to-time-ago-in-php-e-g-1-day-ago-2-days-ago, thanks!
+
+	function timeago($ptime)
+	{
+	    $etime = time() - $ptime;
+
+	    if ($etime < 1)
+	    {
+	        return '0 seconds';
+	    }
+
+	    $a = array( 365 * 24 * 60 * 60  =>  'year',
+	                 30 * 24 * 60 * 60  =>  'month',
+	                      24 * 60 * 60  =>  'day',
+	                           60 * 60  =>  'hour',
+	                                60  =>  'minute',
+	                                 1  =>  'second'
+	                );
+	    $a_plural = array( 'year'   => 'years',
+	                       'month'  => 'months',
+	                       'day'    => 'days',
+	                       'hour'   => 'hours',
+	                       'minute' => 'minutes',
+	                       'second' => 'seconds'
+	                );
+
+	    foreach ($a as $secs => $str)
+	    {
+	        $d = $etime / $secs;
+	        if ($d >= 1)
+	        {
+	            $r = round($d);
+	            return $r . ' ' . ($r > 1 ? $a_plural[$str] : $str) . ' ago';
+	        }
+	    }
 	}
 
 }
--- dashboard.php
+++ dashboard.php
@@ -37,6 +37,117 @@ $Core->requires_auth(true);
 
 $Notes = New Notes($Core->instance);
 
-$Notes->get_notes();
+$username = $Notes->get_username($_SESSION['uid']);
 
 ?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Alphasquare Labs - NotesApp">
+    <meta name="author" content="Sergio Diaz">
+
+    <title>NotesApp - Dash</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="app/css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="app/css/dash.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container" id="post-bar">
+
+    <div class="page-header">
+  		<h1>Hello, <?php echo $username; ?> <small>welcome back to NotesApp</small></h1>
+	</div>
+		<form method='post' class="form-inline" id="postbar">
+
+		<input id="pst" name='content' class="post statusi" placeholder="Write something... ">
+
+		</form>
+
+    	<div class="row">
+
+		<div id="posts">
+		</div>
+		</div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="app/js/bootstrap.min.js"></script>
+		<script>
+
+		function fetch(){
+
+		$.get('fetch.php?data=posts',function(data){
+
+		  $("#posts").html(data);
+
+		});
+
+
+
+		    setTimeout("quasar();",1000);
+
+		}
+
+
+
+		window.onload = fetch();
+
+
+			$("#postbar").submit(function() {
+
+
+
+			    $.ajax({
+
+			           type: "POST",
+
+			           url: 'create.php',
+
+			           data: $("#postbar").serialize(), // serializes the form's elements.
+
+			           beforeSend: function(){
+
+			           $('#pst').prop('disabled', true);
+
+			           },
+
+			           success: function(data)
+
+			           {
+
+			            $.get('fetch.php?data=posts',function(data){
+
+			             $("#posts").html(data);
+
+			            });       
+
+			            $('#pst').prop('disabled', false);
+
+			            $('#pst').val('');
+
+			           }
+
+			         });
+
+
+
+			    return false; // avoid to execute the actual submit of the form.
+
+			});
+		</script>
+    </body>
+</html>
+
