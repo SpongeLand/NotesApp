c69ed8d6563d7932d76e3b8b0d79e1e81a0e4501
Sergio Diaz <Alphapixels@users.noreply.github.com>
2014-11-15 10:39:05 -0400

    Added note deletion method

--- app/custom.php
+++ app/custom.php
@@ -1,5 +1,35 @@
 <?php
 
+/*
+ ________  _______   ________  _____ ______      
+|\_____  \|\  ___ \ |\   __  \|\   _ \  _   \    
+ \|___/  /\ \   __/|\ \  \|\  \ \  \\\__\ \  \   
+     /  / /\ \  \_|/_\ \   __  \ \  \\|__| \  \  
+    /  /_/__\ \  \_|\ \ \  \ \  \ \  \    \ \  \ 
+   |\________\ \_______\ \__\ \__\ \__\    \ \__\
+    \|_______|\|_______|\|__|\|__|\|__|     \|__|
+                                                 
+                                                 
+
+Copyright 2014 Alphasquare.us
+Licensed with the MIT license
+http://github.com/Alphapixels/Zeam
+http://labs.alphasquare.us
+
+You can remove this, but we'd love you to death if you kept this here. 
+Also, we'd marry you if you put a link back to Alphasquare.us.
+
+*/
+
+/**
+*
+* NotesApp
+* @author Sergio Diaz
+* @version 1.0.0
+*
+*/
+
+
 class Notes {
 
 	protected $instance;
@@ -57,6 +87,14 @@ class Notes {
 		$query->execute();
     	$result = $query->get_result();
     	$row = $result->fetch_array(MYSQLI_ASSOC);			
+
+	}
+
+	public function delete($id) {
+
+		$query = $this->instance->prepare("DELETE FROM notes WHERE id=?");
+		$query->bind_param("i", $id);
+		$query->execute();		
 
 	}
 
